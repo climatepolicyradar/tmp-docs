28f01363ec7b5d2175d683542b38277c81a25410
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-04-30 15:55:11 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo "Hello entrypoint 111!"
-# pipenv run fastapi run ./app/main.py --port 8080
+pip3 install -r requirements.txt
 fastapi run ./app/main.py --port 8080
-# NAVIGATOR_DATABASE_URL=postgresql://navigator:navigator@localhost/navigator fastapi run ./app/main.py --port 8080
