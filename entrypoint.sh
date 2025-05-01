cebc05a923844e170f59d4b01e8f5cb8df205156
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-05-01 08:54:46 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Hello entrypoint 111!"
-pip3 install -r requirements.txt
-fastapi run ./app/main.py --port 8888 --host 0.0.0.0
+# pip3 install -r requirements.txt
+# fastapi run ./app/main.py --port 8888 --host 0.0.0.0
